36d019f8d2164a7b680be4459f5937f4959d3004
notsotraumatiq <atiqpatel95@gmail.com>
2020-04-05 19:19:59 -0400

    update

---
+++ productOfArrayExceptSelf.py
@@ -0,0 +1,19 @@
+# 238. Product of Array Except Self
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        array = [1] * len(nums)
+
+        left_sum = 1
+        right_sum = 1
+
+        for i in range(len(nums)):
+            array[i] *= left_sum
+            left_sum *= nums[i]
+
+        for i in reversed(range(len(nums))):
+            array[i] *= right_sum
+            right_sum *= nums[i]
+
+        return array
