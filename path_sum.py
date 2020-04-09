539e23aa456bef76435f642fb08b4d40f4fca0d0
notsotraumatiq <atiqpatel95@gmail.com>
2020-04-08 23:23:40 -0400

    sync

---
+++ path_sum.py
@@ -0,0 +1,29 @@
+# 112. Path Sum
+
+from typing import List
+
+
+# Definition for a binary tree node.
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        return self.maxself(root, sum)
+
+    def maxself(self, root, summer):
+        if not root:
+            return False
+        if not root.left and not root.right and root.val == summer:
+            return True
+
+        summer -= root.val
+
+        self.maxself(root.left, summer)
+        self.maxself(root.right, summer)
+mm
